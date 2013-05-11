b95d490f5517e22bd14a94a2877fa4dfbd64719b
Michael Leaney <leahcimic@gmail.com>
2013-05-11 22:38:39 +1000

    Initial commit

---
+++ choices.js
@@ -0,0 +1,61 @@
+/**
+ * Prompts the user for multiple choice
+ *
+ * `choices` are displayed in an ordered list, user is prompted with message,
+ * callback is returned with the index of the choice.
+ *
+ * @param prompt String Message to show users
+ * @param choices Array[String] Names of the choices
+ * @param callback Function Callback the function when a valid choice is made
+ *
+ * @install
+ * npm install choices
+ *
+ * @example
+ *
+ * var choices = require('choices');
+ * var options = ['First Option', 'Second option', 'Third option'];
+ * choices('Pick an option', options, function(idx) {
+ *   console.log('You picked ' + options[idx]);
+ * });
+ *
+ *   1: First option
+ *   2: Second option
+ *   3: Third option
+ * Pick an option>> 2
+ * You picked Second option
+ *
+ */
+
+module.exports = function(prompt, choices, callback) {
+  var stdin = process.stdin;
+  choices.forEach(function(name, idx) {
+    console.log('  ' + (idx + 1) + ': ' + name);
+  });
+  process.stdout.write(prompt + '>> ');
+  stdin.setRawMode(true);
+  stdin.resume();
+
+  stdin.once(
+    'data',
+    function(key) {
+      var idx;
+      stdin.pause();
+      stdin.setRawMode(false);
+
+      if (key === '\u0003')
+        process.exit();
+
+      console.log(key);
+
+      idx = +key;
+
+      if (idx > 0 && idx <= choices.length)
+        return callback(idx - 1);
+
+      process.nextTick(function() {
+        choices(prompt, choices, callback);
+      });
+    }
+  );
+};
